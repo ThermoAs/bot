9aa88688be473475163b45b24dbc063d7b2819d5
ThermoAs <marcin.urban369@gmail.com>
2026-03-05 09:05:03 +0100

    Create config.py\

---
+++ config.py
@@ -0,0 +1,20 @@
+"""
+Plik konfiguracyjny projektu.
+
+Zawiera:
+- listę fraz do researchu
+- konfigurację Google Sheets
+"""
+
+# Frazy do wyszukiwania na Allegro
+SEARCH_PHRASES = [
+    "mata do jogi",
+    "organizer do szuflady",
+    "lampka nocna led",
+]
+
+# Google Sheets
+GOOGLE_SHEETS_NAME = "allegro_product_research"
+
+# Ścieżka do pliku credentials Google API
+GOOGLE_CREDS_FILE = "credentials.json"
